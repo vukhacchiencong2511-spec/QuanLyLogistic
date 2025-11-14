0eabff062c373af42111db6fa389ac113b717aed
Lâm <wibuchua36@gmail.com>
2025-11-14 16:54:45 +0700

    Update GiaoDichCOD.cs

--- QuanLyLogisticsApi/Models/GiaoDichCOD.cs
+++ QuanLyLogisticsApi/Models/GiaoDichCOD.cs
@@ -1,14 +1,14 @@
-﻿namespace QuanLyLogisticsApi.Models
+namespace QuanLyLogisticsApi.Models
 {
     public class GiaoDichCOD
     {
-        public long MaGiaoDich { get; set; }
+        public long? MaGiaoDich { get; set; }
         public string MaDon { get; set; }
         public decimal SoTien { get; set; }
         public string NguoiThu { get; set; }
-        public DateTime? NgayThu { get; set; }
+        public DateTime? NgayThu { get; set; } = DateTime.Now;
         public bool DaDoiSoat { get; set; }
-        public DateTime? NgayDoiSoat { get; set; }
+        public DateTime? NgayDoiSoat { get; set; } = DateTime.Now;
         public decimal? SoTienThanhToan { get; set; }
         public string DuLieuThem { get; set; }
     }
