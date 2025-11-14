da4fa111549fb5757aa8c4b8e6e4e936604d4f57
Lâm <wibuchua36@gmail.com>
2025-11-14 16:49:26 +0700

    Update DiemDungDAL.cs

--- QuanLyLogisticsApi/DAL/DiemDungDAL.cs
+++ QuanLyLogisticsApi/DAL/DiemDungDAL.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.SqlClient;
+using System.Data.SqlClient;
 using QuanLyLogisticsApi.Models;
 
 namespace QuanLyLogisticsApi.DAL
@@ -22,7 +22,7 @@ namespace QuanLyLogisticsApi.DAL
             {
                 list.Add(new DiemDung
                 {
-                    MaDiemDung = Convert.ToInt32(dr["MaDiemDung"]),
+                    MaDiemDung = dr["MaDiemDung"].ToString(),
                     MaTuyen = dr["MaTuyen"].ToString(),
                     ThuTuDung = Convert.ToInt32(dr["ThuTuDung"]),
                     MaDon = dr["MaDon"].ToString(),
@@ -37,8 +37,9 @@ namespace QuanLyLogisticsApi.DAL
         {
             using SqlConnection conn = new(_conn);
             SqlCommand cmd = new(@"INSERT INTO DiemDung 
-                (MaTuyen, ThuTuDung, MaDon, DuKienDen, ThucTeDen)
-                VALUES (@tuyen, @thu, @don, @dukien, @thucte)", conn);
+                (MaDiemDung, MaTuyen, ThuTuDung, MaDon, DuKienDen, ThucTeDen)
+                VALUES (@ma, @tuyen, @thu, @don, @dukien, @thucte)", conn);
+            cmd.Parameters.AddWithValue("@ma", d.MaDiemDung);
             cmd.Parameters.AddWithValue("@tuyen", d.MaTuyen);
             cmd.Parameters.AddWithValue("@thu", d.ThuTuDung);
             cmd.Parameters.AddWithValue("@don", d.MaDon);
@@ -68,6 +69,29 @@ namespace QuanLyLogisticsApi.DAL
             cmd.Parameters.AddWithValue("@id", id);
             conn.Open();
             return cmd.ExecuteNonQuery() > 0;
+        }
+
+        public DiemDung? GetById(string id)
+        {
+            using SqlConnection conn = new(_conn);
+            string sql = "SELECT * FROM DiemDung WHERE MaDiemDung = @id";
+            SqlCommand cmd = new(sql, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            conn.Open();
+            using SqlDataReader r = cmd.ExecuteReader();
+            if (r.Read())
+            {
+                return new DiemDung
+                {
+                    MaDiemDung = r["MaDiemDung"].ToString(),
+                    MaTuyen = r["MaTuyen"].ToString(),
+                    ThuTuDung = Convert.ToInt32(r["ThuTuDung"]),
+                    MaDon = r["MaDon"].ToString(),
+                    DuKienDen = r["DuKienDen"] as DateTime?,
+                    ThucTeDen = r["ThucTeDen"] as DateTime?
+                };
+            }
+            return null;
         }
     }
 }
