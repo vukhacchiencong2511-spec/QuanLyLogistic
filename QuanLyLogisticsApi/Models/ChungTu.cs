fe88367b51ae2192cd3400f8ffce0cf28d613a2f
Lâm <wibuchua36@gmail.com>
2025-11-14 16:53:16 +0700

    Update ChungTu.cs

--- QuanLyLogisticsApi/Models/ChungTu.cs
+++ QuanLyLogisticsApi/Models/ChungTu.cs
@@ -1,14 +1,13 @@
-﻿namespace QuanLyLogisticsApi.Models
+namespace QuanLyLogisticsApi.Models
 {
     public class ChungTu
     {
-        public long MaChungTu { get; set; }
+        public long? MaChungTu { get; set; }
         public string MaDon { get; set; }
         public string NguoiUpload { get; set; }
-        public DateTime NgayUpload { get; set; }
+        public DateTime? NgayUpload { get; set; } = DateTime.Now;
         public string KyNhan { get; set; }
         public string DuongDanThuNho { get; set; }
         public string LoaiKyNhan { get; set; }
     }
 }
-
