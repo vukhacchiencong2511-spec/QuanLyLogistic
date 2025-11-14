6de33a1edb87bb6c320c8d57d7f8ac10ef2bc2ba
Lâm <wibuchua36@gmail.com>
2025-11-14 16:45:37 +0700

    Update DiemDungController.cs

--- QuanLyLogisticsApi/Controllers/DiemDungController.cs
+++ QuanLyLogisticsApi/Controllers/DiemDungController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using QuanLyLogisticsApi.BUS;
 using QuanLyLogisticsApi.Models;
 
@@ -40,6 +41,46 @@ namespace QuanLyLogisticsApi.Controllers
             if (_bus.Delete(id))
                 return Ok(new { message = "Xóa thành công" });
             return BadRequest(new { message = "Lỗi khi xóa điểm dừng" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var item = _bus.GetById(id);
+            if (item == null) return NotFound(new { message = "Không tìm thấy Điểm dừng." });
+            return Ok(item);
+        }
+
+        [HttpGet("export-excel")]
+        public IActionResult ExportExcel()
+        {
+            using var pkg = new ExcelPackage();
+            var ws = pkg.Workbook.Worksheets.Add("DiemDung");
+
+            ws.Cells[1, 1].Value = "Mã điểm dừng";
+            ws.Cells[1, 2].Value = "Mã tuyến";
+            ws.Cells[1, 3].Value = "Thứ tự dừng";
+            ws.Cells[1, 4].Value = "Mã đơn";
+            ws.Cells[1, 5].Value = "Dự kiến đến";
+            ws.Cells[1, 6].Value = "Thực tế đến";
+
+            var list = _bus.GetAll();
+            int r = 2;
+
+            foreach (var x in list)
+            {
+                ws.Cells[r, 1].Value = x.MaDiemDung;
+                ws.Cells[r, 2].Value = x.MaTuyen;
+                ws.Cells[r, 3].Value = x.ThuTuDung;
+                ws.Cells[r, 4].Value = x.MaDon;
+                ws.Cells[r, 5].Value = x.DuKienDen;
+                ws.Cells[r, 6].Value = x.ThucTeDen;
+                r++;
+            }
+
+            return File(pkg.GetAsByteArray(),
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+              "DiemDung.xlsx");
         }
     }
 }
