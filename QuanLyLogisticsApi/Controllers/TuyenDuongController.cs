63bff000a10bb449b63fd0394f97e31cf1be07b7
Lâm <wibuchua36@gmail.com>
2025-11-14 16:47:56 +0700

    Update TuyenDuongController.cs

--- QuanLyLogisticsApi/Controllers/TuyenDuongController.cs
+++ QuanLyLogisticsApi/Controllers/TuyenDuongController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using QuanLyLogisticsApi.BUS;
 using QuanLyLogisticsApi.Models;
 
@@ -40,6 +41,51 @@ namespace QuanLyLogisticsApi.Controllers
             if (_bus.Delete(id))
                 return Ok(new { message = "Xóa thành công" });
             return BadRequest(new { message = "Lỗi khi xóa tuyến đường" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var item = _bus.GetById(id);
+            if (item == null) return NotFound(new { message = "Không tìm thấy tuyến đường." });
+            return Ok(item);
+        }
+
+        [HttpGet("export-excel")]
+        public IActionResult ExportExcel()
+        {
+            using var pkg = new ExcelPackage();
+            var ws = pkg.Workbook.Worksheets.Add("TuyenDuong");
+
+            string[] cols = {
+        "Mã tuyến","Mã tuyến code","Mã tài xế","Phương tiện",
+        "Thười gian bắt đầu","Thời gian kết thúc","Mã khu vực",
+        "Doanh thu ước tính","Ngày tạo"
+    };
+
+            for (int i = 0; i < cols.Length; i++)
+                ws.Cells[1, i + 1].Value = cols[i];
+
+            var list = _bus.GetAll();
+            int r = 2;
+
+            foreach (var t in list)
+            {
+                ws.Cells[r, 1].Value = t.MaTuyen;
+                ws.Cells[r, 2].Value = t.MaTuyenCode;
+                ws.Cells[r, 3].Value = t.MaTaiXe;
+                ws.Cells[r, 4].Value = t.PhuongTien;
+                ws.Cells[r, 5].Value = t.ThoiGianBatDau;
+                ws.Cells[r, 6].Value = t.ThoiGianKetThuc;
+                ws.Cells[r, 7].Value = t.MaKhuVuc;
+                ws.Cells[r, 8].Value = t.DoanhThuUocTinh;
+                ws.Cells[r, 9].Value = t.NgayTao;
+                r++;
+            }
+
+            return File(pkg.GetAsByteArray(),
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+              "TuyenDuong.xlsx");
         }
     }
 }
