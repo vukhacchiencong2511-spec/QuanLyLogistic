899529931444e541cbf354166ac73e11e27c6944
Lâm <wibuchua36@gmail.com>
2025-11-14 16:57:02 +0700

    Add files via upload

---
+++ QuanLyLogisticsApi/Models/VanDonDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace QuanLyLogisticsApi.Models
+{
+    public class VanDonDetailDTO
+    {
+        public string SoVanDon { get; set; }
+        public string ThongTinNhaXe { get; set; }
+        public string TrangThai { get; set; }
+        public string LoaiHang { get; set; }
+        public string TenKhachGui { get; set; }
+        public string TenKhachNhan { get; set; }
+        public string DiaChiLay { get; set; }
+        public string DiaChiGiao { get; set; }
+        public string MaTuyen { get; set; }
+        public decimal GiaTriKhaiBao { get; set; }
+        public string ChungTu { get; set; }
+        public DateTime NgayTao { get; set; }
+    }
+}
