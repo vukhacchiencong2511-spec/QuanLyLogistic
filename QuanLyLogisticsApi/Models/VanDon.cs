7627e821b3ed552a03e813f94c66c0ce62ff8f61
Lâm <wibuchua36@gmail.com>
2025-11-14 16:56:18 +0700

    Update VanDon.cs

--- QuanLyLogisticsApi/Models/VanDon.cs
+++ QuanLyLogisticsApi/Models/VanDon.cs
@@ -1,11 +1,11 @@
-﻿namespace QuanLyLogisticsApi.Models
+namespace QuanLyLogisticsApi.Models
 {
     public class VanDon
     {
         public string MaVanDon { get; set; }
         public string SoVanDon { get; set; }
         public string MaDon { get; set; }
-        public DateTime NgayPhatHanh { get; set; }
-        public string ThongTinNhaXe { get; set; }
+        public DateTime? NgayPhatHanh { get; set; } = DateTime.Now;
+        public string ThongTinNhaXe { get; set; }     
     }
 }
