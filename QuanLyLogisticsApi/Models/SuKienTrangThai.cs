500aa5d91773f22b223dfe3b134ec4fd2a0ebbfa
Lâm <wibuchua36@gmail.com>
2025-11-14 16:55:40 +0700

    Update SuKienTrangThai.cs

--- QuanLyLogisticsApi/Models/SuKienTrangThai.cs
+++ QuanLyLogisticsApi/Models/SuKienTrangThai.cs
@@ -1,16 +1,16 @@
-﻿namespace QuanLyLogisticsApi.Models
+namespace QuanLyLogisticsApi.Models
 {
     public class SuKienTrangThai
     {
-        public long MaSuKien { get; set; }
+        public long? MaSuKien { get; set; }
         public string MaDon { get; set; }
         public string TrangThai { get; set; }
         public string LyDo { get; set; }
-        public DateTime ThoiGian { get; set; }
+        public DateTime? ThoiGian { get; set; } = DateTime.Now;
         public string NguoiCapNhat { get; set; }
         public string DuLieuThem { get; set; }
         public string MaSuKienNgoai { get; set; }
         public string KhoaIdempotent { get; set; }
-        public DateTime NgayTao { get; set; }
+        public DateTime? NgayTao { get; set; } = DateTime.Now;
     }
 }
