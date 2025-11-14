c3e77ba0cb72c4f4d98585b87de1586dcfc41134
Lâm <wibuchua36@gmail.com>
2025-11-14 16:45:16 +0700

    Update DiaChiController.cs

--- QuanLyLogisticsApi/Controllers/DiaChiController.cs
+++ QuanLyLogisticsApi/Controllers/DiaChiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using QuanLyLogisticsApi.BUS;
 using QuanLyLogisticsApi.Models;
 
@@ -40,6 +41,47 @@ namespace QuanLyLogisticsApi.Controllers
             if (_bus.Delete(id))
                 return Ok(new { message = "Xóa thành công" });
             return BadRequest(new { message = "Lỗi khi xóa" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var diaChi = _bus.GetById(id);
+            if (diaChi == null)
+                return NotFound(new { message = "Không tìm thấy địa chỉ." });
+            return Ok(diaChi);
+        }
+
+        [HttpGet("export-excel")]
+        public IActionResult ExportExcel()
+        {
+            using var pkg = new ExcelPackage();
+            var ws = pkg.Workbook.Worksheets.Add("DiaChi");
+
+            ws.Cells[1, 1].Value = "Mã địa chỉ";
+            ws.Cells[1, 2].Value = "Mã khách hàng";
+            ws.Cells[1, 3].Value = "Địa chỉ chi tiết";
+            ws.Cells[1, 4].Value = "Thành phố";
+            ws.Cells[1, 5].Value = "Phường";
+            ws.Cells[1, 6].Value = "Mã bưu điện";
+
+            var list = _bus.GetAll();
+            int r = 2;
+
+            foreach (var x in list)
+            {
+                ws.Cells[r, 1].Value = x.MaDiaChi;
+                ws.Cells[r, 2].Value = x.MaKhachHang;
+                ws.Cells[r, 3].Value = x.DiaChiChiTiet;
+                ws.Cells[r, 4].Value = x.ThanhPho;
+                ws.Cells[r, 5].Value = x.QuanHuyen;
+                ws.Cells[r, 6].Value = x.MaBuuDien;
+                r++;
+            }
+
+            return File(pkg.GetAsByteArray(),
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "DiaChi.xlsx");
         }
     }
 }
