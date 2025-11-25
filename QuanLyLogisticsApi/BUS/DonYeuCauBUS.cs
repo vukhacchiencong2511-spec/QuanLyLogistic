1db31fee2f1bc4b54c3eb067bef64f0782d8c15b
Lâm <wibuchua36@gmail.com>
2025-11-25 16:32:02 +0700

    Add files via upload

---
+++ QuanLyLogisticsApi/BUS/DonYeuCauBUS.cs
@@ -0,0 +1,28 @@
+﻿using QuanLyLogisticsApi.DAL;
+using QuanLyLogisticsApi.Models;
+
+namespace QuanLyLogisticsApi.BUS
+{
+    public class DonYeuCauBUS
+    {
+        private readonly DonYeuCauDAL _dal;
+
+        public DonYeuCauBUS(IConfiguration config)
+        {
+            _dal = new DonYeuCauDAL(config);
+        }
+
+        public List<DonYeuCau> GetAll() => _dal.GetAll();
+        public DonYeuCau GetById(string id) => _dal.GetById(id);
+
+        public bool Add(DonYeuCau d)
+        {
+            d.MaYeuCau = _dal.GenerateNewId();
+            return _dal.Add(d);
+        }
+
+        public bool Delete(string id) => _dal.Delete(id);
+
+        public List<DonYeuCau> Search(string key) => _dal.Search(key);
+    }
+}
