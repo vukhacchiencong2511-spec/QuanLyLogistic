0cd135fcb0822a153a7a4043b2e5409ef839b7b6
Lâm <wibuchua36@gmail.com>
2025-11-25 16:24:25 +0700

    Add files via upload

---
+++ QuanLyLogisticsApi/Models/ChangePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanLyLogisticsApi.Models
+{
+    public class ChangePasswordDTO
+    {
+        public string MaNguoiDung { get; set; }
+        public string MatKhauCu { get; set; }
+        public string MatKhauMoi { get; set; }
+    }
+}
---
+++ QuanLyLogisticsApi/Models/DonYeuCau.cs
@@ -0,0 +1,23 @@
+﻿namespace QuanLyLogisticsApi.Models
+{
+    public class DonYeuCau
+    {
+        public string MaYeuCau { get; set; }
+        public string TenNguoiGui { get; set; }
+        public string SDTNguoiGui { get; set; }
+        public string EmailNguoiGui { get; set; }
+        public string DiaChiGui { get; set; }
+
+        public string TenNguoiNhan { get; set; }
+        public string SDTNguoiNhan { get; set; }
+        public string EmailNguoiNhan { get; set; }
+        public string DiaChiNhan { get; set; }
+
+        public string LoaiHang { get; set; }
+        public decimal KhoiLuong { get; set; }
+        public decimal GiaTriKhaiBao { get; set; }
+        public string GhiChu { get; set; }
+
+        public DateTime? NgayTao { get; set; } = DateTime.Now;
+    }
+}
